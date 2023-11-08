51c7ec2a90f73feb50f0a0882a9b55ab53727e72
Karko93 <marco.ketzel@hotmail.com>
2023-11-08 10:56:50 +0100

    Add config class for plots

---
+++ src/moveroplot/config/plot_settings.py
@@ -0,0 +1,13 @@
+class PlotSettings:
+    linecolors: list[str] = [
+        "black",
+        "red",
+        "blue",
+        "green",
+        "cyan",
+        "yellow",
+        "magenta",
+        "orange",
+    ]
+
+    line_styles: list[str] = ["-", ":", "--", "-."]
