29338cc94c178b404c09dc9121c350eb1cbd6c0e
Karko93 <marco.ketzel@hotmail.com>
2023-12-20 13:01:08 +0100

    Add exception when date/file is invalid

--- src/moveroplot/load_files.py
+++ src/moveroplot/load_files.py
@@ -2,6 +2,7 @@
 
 # Standard library
 import re
+from datetime import datetime
 from pathlib import Path
 
 # Local
@@ -9,6 +10,15 @@ from .utils.atab import Atab
 
 
 # pylint: disable=too-many-arguments,too-many-locals
+def is_valid_data(header):
+    try:
+        datetime.strptime(" ".join(header["Start time"][0:3:2]), "%Y-%m-%d %H:%M")
+        datetime.strptime(" ".join(header["End time"][0:2]), "%Y-%m-%d %H:%M")
+        return True
+    except ValueError:
+        return False
+
+
 def load_relevant_files(
     input_dir,
     file_prefix,
@@ -45,17 +55,20 @@ def load_relevant_files(
                     df = loaded_atab.data
                     if transform_func:
                         df = transform_func(df, header)
-                    # add information to dict
-                    first_key, second_key = (
-                        (lt_range, model) if ltr_first else (model, lt_range)
-                    )
-                    corresponding_files_dict.setdefault(first_key, {})[second_key] = {
-                        "header": header,
-                        "df": df,
-                    }
+                    if is_valid_data(header):
+                        # add information to dict
+                        first_key, second_key = (
+                            (lt_range, model) if ltr_first else (model, lt_range)
+                        )
+                        corresponding_files_dict.setdefault(first_key, {})[
+                            second_key
+                        ] = {
+                            "header": header,
+                            "df": df,
+                        }
 
-                    # add path of file to list of relevant files
-                    files_list.append(file_path)
+                        # add path of file to list of relevant files
+                        files_list.append(file_path)
 
     if debug:
         print(f"\nFor parameter: {parameter} these files are relevant:\n")
--- src/moveroplot/station_scores.py
+++ src/moveroplot/station_scores.py
@@ -87,8 +87,6 @@ def _add_plot_text(ax, data, score, ltr):
     min_station = data["df"].loc[score].idxmin()
     max_value = data["df"].loc[score].max()
     max_station = data["df"].loc[score].idxmax()
-    start_date = datetime(9999, 1, 1, hour=0, minute=0)
-    end_date = datetime(9999, 1, 1, hour=0, minute=0)
     try:
         start_date = datetime.strptime(
             " ".join(data["header"]["Start time"][0:3:2]), "%Y-%m-%d %H:%M"
@@ -96,7 +94,9 @@ def _add_plot_text(ax, data, score, ltr):
         end_date = datetime.strptime(
             " ".join(data["header"]["End time"][0:2]), "%Y-%m-%d %H:%M"
         )
-    finally:
+    except ValueError:
+        start_date = datetime(9999, 1, 1, hour=0, minute=0)
+        end_date = datetime(9999, 1, 1, hour=0, minute=0)
         print("Found invalid date format.")
 
     # pylint: disable=line-too-long
