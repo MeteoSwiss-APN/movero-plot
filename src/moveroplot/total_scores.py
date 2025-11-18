035dac54f9f5abcf3907cca07373e0a5b89c9adc
Peider Könz <peider.koenz@meteoswiss.ch>
2025-11-18 15:13:06 +0100

    adjust colorbar settings' system

--- src/moveroplot/total_scores.py
+++ src/moveroplot/total_scores.py
@@ -18,7 +18,7 @@ from .plotting import get_total_dates_from_headers
 from .utils.parse_plot_synop_ch import cat_total_score_range
 from .utils.parse_plot_synop_ch import total_score_range
 from .utils.set_ylims import set_ylim
-from.utils.unitless_scores_lists import unit_number_scores, unitless_scores
+from.utils.scores_lists_settings import unit_number_scores, unitless_scores
 
 # pylint: enable=no-name-in-module
 
